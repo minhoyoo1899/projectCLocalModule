acdec5e1a11db29b5b64f3e37565a88705ce6185
minhoyoo <minhoyoo1899@gmai.com>
2022-10-23 01:58:35 +0900

    connect to db & api by express

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
 import express from 'express';
 import path from 'path';
+import mysql from 'mysql';
+import request from 'request';
+
+//import dbconfig from './build/db.js'
+
+//const dbconfig = require('./build/db.js');
+
+const dbconfig = {
+  host: 'localhost',
+  user: 'root',
+  password: '1234',
+  port:'3306',
+  database: 'inthem'
+}
+
+//console.log(dbconfig);
+
+const connection = mysql.createConnection(dbconfig);
+
 
 //const express = require('express');
 
@@ -9,50 +28,8 @@ app.set('port', process.env.PORT || 3000);
 
 const __dirname = path.resolve();
 
-// const indexRouter = (addr) => {
-//   app.get(addr, (req, res) => {
-//     res.sendFile(path.join(__dirname, addr));
-//   });
-// }
-// //index
-// indexRouter('/source/img/bg_dpimg.png');
-// indexRouter('/source/vod/tekken8.mp4');
-// indexRouter('/public/js/index.js');
-// indexRouter('/public/js/action/vodEvent.js');
-// indexRouter('/public/js/style/indexCss.js');
-// indexRouter('/public/js/style/style.js');
-// indexRouter('/public/css/index.css');
-
-// //administrator
-// indexRouter('/public/css/admin.css');
-// indexRouter('/public/js/style/admin.js');
-// indexRouter('/source/adimg/admin-icon02.png');
-// indexRouter('/source/adimg/upMedia.png');
-// indexRouter('/source/adimg/visiors_graph.png');
-// indexRouter('/source/adimg/board.png');
-// indexRouter('/source/adimg/good.png');
-// indexRouter('/source/adimg/click.png');
-// indexRouter('/source/adimg/views.png');
-
-
-// //clientInfo aside
-// indexRouter('/public/css/clientInfo/clientInfo.css');
-// indexRouter('/public/js/style/clientInfo/myFavoMov.js');
-// indexRouter('/public/js/style/clientInfo/styleMaker.js');
-// indexRouter('/source/img/client_info_icon.png');
-
-//clientInfo main img //clientInfo main img //clientInfo main img
-// indexRouter('/source/img/client_info__movie1.png');
-// indexRouter('/source/img/client_info__movie2.png');
-// indexRouter('/source/img/client_info__movie3.png');
-// indexRouter('/source/img/client_info__movie4.png');
-// indexRouter('/source/img/client_info__movie5.png');
-// indexRouter('/source/img/client_info__movie6.png');
-// indexRouter('/source/img/client_info__pentagon.png');
-
 app.use('/public', express.static(__dirname + '/public'));
 app.use('/source', express.static(__dirname + '/source'));
-
 
 app.get('/', (req, res) => { 
   //res.send('Hello, Express');
@@ -91,10 +68,6 @@ app.get('/textContent', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/views/board/textContent.html'));
 });
 
-// app.get('/generalForum', (req, res) => {
-//   res.sendFile(path.join(__dirname, '/public/views/board/generalForum.html'));
-// });
-
 app.get('/logIn', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/views/logIn/login.html'));
 });
@@ -102,10 +75,6 @@ app.get('/logIn', (req, res) => {
 app.get('/movieInfo', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/views/movieInfo/movieInfor.html'));
 });
-
-// app.get('/myBoardInfo', (req, res) => {
-//   res.sendFile(path.join(__dirname, '/public/views/clientInfo/myBoard/myboard.html'));
-// });
 
 app.get('/boardHead', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/views/clientInfo/myBoard/myboardHead.html'));
@@ -121,6 +90,82 @@ app.get('/slider', (req, res) => {
 
 app.get('/randomMovie', (req, res) => { 
   res.sendFile(path.join(__dirname, '/public/views/randomMovie/randomMovie.html'));
+});
+
+app.get('/dbPage', (req, res) => { 
+  connection.query('SELECT * FROM director', (error, rows) => {
+    if (error) throw error;
+    res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>In-The-M-ovie- Express!</title>    
+    </head>
+    <body>
+        <div id="app">
+         rows : ${rows} <br/>
+         rows[0] :${rows[0]} <br/>
+         rows[0].DIRECTOR_SEQ :  ${rows[0].DIRECTOR_SEQ} <br/>
+         rows[0].DIRECTOR_NAME : ${rows[0].DIRECTOR_NAME} <br/>
+         rows[0].DIRECTOR_CODE : ${rows[0].DIRECTOR_CODE} <br/>
+        </div>        
+    </body>
+    </html>`);
+    //console.log(`data : ${rows}`);
+    console.log(rows);
+    console.dir(rows);
+    console.dir(rows[0]);
+    console.dir(rows[0].DIRECTOR_SEQ);
+    //console.dir(typeof rows);
+    //console.dir(Array.isArray(rows));
+  });
+});
+
+app.get('/dbtest', (req, res) => {
+  connection.query('SELECT * FROM director', (error, rows) => {
+    if (error) throw error;
+    console.log(`data : ${rows}`);    
+    res.send(rows);
+  });
+});
+
+app.get('/api', (req, res) => {
+  const apiURL = 'https://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp?';  
+  const apiKey = '7204O8KH4D5547Q3JBB7';
+  const genre = "로맨스";
+  const startDate = 20220222;
+  const kmdb_url = `https://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp?collection=kmdb_new2&ServiceKey=7204O8KH4D5547Q3JBB7&detail =Y&genre=${encodeURI(genre)}&releaseDts=20221009&listCount=50`;
+
+  const options = {
+    method: 'GET',
+    url: kmdb_url,
+    headers: {
+      Cookie: 'JSESSIONID=E5F2C3985D522765327DD7D107C07129'
+    }
+  }
+
+  request(options, function (error, response) {
+    if (error) throw new Error(error);
+    console.log(response.body);
+    res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>In-The-M-ovie- Express!</title>    
+    </head>
+    <body>
+        <div id="app">
+         api : ${response.body}
+        </div>        
+    </body>
+    </html>`);  
+  });
 });
 
 
