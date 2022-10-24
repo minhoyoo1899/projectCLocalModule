d2a004e6faba35323e4233aa86a93a5ef6b73e0c
MinhoYoo <minhoyoo1899@gmail.com>
2022-10-24 17:40:22 +0900

    mapping

--- public/js/style/clientInfo/myBoard/myboard.js
+++ public/js/style/clientInfo/myBoard/myboard.js
@@ -27,6 +27,24 @@ styleMaker.tagMaker(div1, "div", "p", "닉네임", 1 );
 styleMaker.tagMaker(div2, "div", "p", "내 정보 수정", 1);
 styleMaker.tagMaker(div2, "div", "p", "내가 찜한 영화", 1);
 styleMaker.tagMaker(div2, "div", "p", "내가 쓴 글", 1);
+
+for (let i = 0; i < div2.children.length; i++) {
+  const target = div2.children[i];
+  target.addEventListener('click', (event) => {
+    switch (i) {
+      case 0:
+        location.href = '/myInfo';
+        break;
+      case 1:
+        location.href = '/myFavoMov';
+        break;
+      
+      case 2:
+        location.href = '/myBoard';
+        break;
+    }
+  });
+}
 // -------------------------app > aside > div2 > (div > p) *3--------------------
 
 
